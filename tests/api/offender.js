7657596e1480781829db0b6d19790acd875ad8fb
Matt Smith <mattonfoot@gmail.com>
2017-04-05 11:13:50 +0100

    updated Validation Tests

--- tests/api/offender.js
+++ tests/api/offender.js
@@ -26,10 +26,28 @@ describe('api', () => {
 
         });
 
-        it('should return a 409 response when the nomsId is invalid', (done) => {
+        it('should return a 409 response when the nomsId is a plain string', (done) => {
 
           request(server)
             .get('/offender/BANG/viper')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(409)
+            .end((err, res) => {
+              should.not.exist(err);
+
+              res.body.should.have.property('code', 'InvalidArgument');
+              res.body.should.have.property('message', 'nomsId (INVALID): Invalid characters');
+
+              done();
+            });
+
+        });
+
+        it('should return a 409 response when the nomsId is missing a digit', (done) => {
+
+          request(server)
+            .get('/offender/A123BC/viper')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(409)
