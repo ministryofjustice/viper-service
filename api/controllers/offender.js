8e53a83af7f4f01bdc2e06374074a237ad459edc
mikejackson <michael.jackson@digital.justice.gov.uk>
2017-06-19 15:47:44 +0100

    Convert callback into promise

--- api/controllers/offender.js
+++ api/controllers/offender.js
@@ -1,63 +1,65 @@
-const restify = require('restify');
-const reader = require('../datasources/reader');
-const cache = {};
+const restify = require('restify')
+const reader = require('../datasources/reader')
+const cache = {}
 
 const asJson = (res, obj) =>
-  res.send(obj);
+  res.send(obj)
 
 const withParam = (req, key) =>
-  (req.swagger.params[key] && req.swagger.params[key].value);
+  (req.swagger.params[key] && req.swagger.params[key].value)
 
 const withBody = (req, key) =>
-  (req.swagger.params.body && req.swagger.params.body.value && req.swagger.params.body.value[key]);
+  (req.swagger.params.body && req.swagger.params.body.value && req.swagger.params.body.value[key])
 
 const successfulViperRating = (nomsId, viperRating) =>
   ({
     nomsId: nomsId,
     viperRating: 1 * viperRating,
-  });
-
-const retrieveViperRating = (nomsId, db, cb) => {
-  reader.read(nomsId, db, cb);
-  // switch (nomsId) {
-  //   case 'A1234BC': cb(0.56);
-  //   case 'C4321BA': cb();
-  //   case 'A3163JU': reader.read(nomsId, db, cb); break
-  //   default:        cb (cache[nomsId] = cache[nomsId] || Math.random().toFixed(2));
-  // }
-};
+  })
 
 module.exports.retrieveViperRating = (req, res, next) => {
-  console.log(req);
+  console.log(req)
 
-  function cb(viperRating) {
+  var nomsId = withParam(req, 'nomsId')
+
+  function handleResult(viperRating) {
     if (!viperRating) {
-      return next(new restify.ResourceNotFoundError(`/offender/${nomsId}/viper does not exist`));
+      next(new restify.ResourceNotFoundError(`/offender/${nomsId}/viper does not exist`))
+    } else {
+      asJson(res, successfulViperRating(nomsId, viperRating))
     }
-    asJson(res, successfulViperRating(nomsId, viperRating));
-    next();
   }
 
-  var nomsId = withParam(req, 'nomsId');
-  retrieveViperRating(nomsId, req.config.db, cb);
+  reader.read(nomsId, req.config.db)
+    .then(handleResult, (err) => {throw err})
 
-};
+  // function cb(viperRating) {
+  //   if (!viperRating) {
+  //     return next(new restify.ResourceNotFoundError(`/offender/${nomsId}/viper does not exist`));
+  //   }
+  //   asJson(res, successfulViperRating(nomsId, viperRating));
+  //   next();
+  // }
+
+  // retrieveViperRating(nomsId, req.config.db, cb);
+
+}
 
 module.exports.recordViperRating = (req, res, next) => {
-  var nomsId = withParam(req, 'nomsId');
-  var viperRating = withBody(req, 'viperRating');
+  var nomsId = withParam(req, 'nomsId')
+  var viperRating = withBody(req, 'viperRating')
 
   if (!nomsId) {
-    return next(new restify.InvalidArgumentError('nomsId is required'));
+    return next(new restify.InvalidArgumentError('nomsId is required'))
   }
 
   if (!viperRating) {
-    return next(new restify.InvalidArgumentError('viperRating is required'));
+    return next(new restify.InvalidArgumentError('viperRating is required'))
   }
 
-  cache[nomsId] = viperRating;
+  cache[nomsId] = viperRating
 
-  res.send(201);
+  res.send(201)
 
-  next();
-};
+  next()
+}
--- api/datasources/reader.js
+++ api/datasources/reader.js
@@ -1,14 +1,17 @@
-var __ = require('underscore');
-require('require-sql');
+var __ = require('underscore')
+require('require-sql')
 
-var query = require('../sql/query_score.sql');
+var queryTemplate = __.template(require('../sql/query_score.sql'))
 
-module.exports.read = (id, db, cb) => {
-  var queryWithParameters = __.template(query)({nomis_id: id});
-  return db.exec(queryWithParameters, (err, rows) => {
-    if(err) {
-      throw err;
-    }
-    cb(rows.length == 0 ? null : rows [0].SCORE)
-  });
+module.exports.read = (id, db) => {
+  var queryWithParameters = queryTemplate({nomis_id: id})
+  return new Promise(function (resolve, reject) {
+    db.exec(queryWithParameters, (err, rows) => {
+      if (err) {
+        reject(err)
+      }
+
+      resolve(rows.length == 0 ? null : rows [0].SCORE)
+    })
+  })
 }
