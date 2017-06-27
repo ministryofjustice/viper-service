1f7786c724eabdac2ce0a252b35f254aa9ac6d1c
Glen Mailer <glenjamin@gmail.com>
2017-06-27 09:54:44 +0100

    Setup MsSQL DB on CI and run dredd tests end-to-end

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,14 @@
 /**
  * This file is automatically loaded when knex runs migrations
  */
+const _ = require('lodash');
 
 const config = require('./server/config');
 
+
 module.exports = {
   client: 'mssql',
-  connection: Object.assign({}, config.db, {
+  connection: _.defaultsDeep(config.db, {
     options: {
       encrypt: true,
     },
