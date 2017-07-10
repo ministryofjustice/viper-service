10f4123860ac510e4410b83e9371565a7096252d
Glen Mailer <glenjamin@gmail.com>
2017-07-10 14:51:06 +0100

    Add application insights to record bunyan and SQL logs

--- server/log.js
+++ server/log.js
@@ -3,8 +3,11 @@ const Logger = require('bunyan');
 
 const config = require('./config');
 
+// applicationinsights automatically collects bunyan logs
+require('./azure-appinsights');
+
 module.exports = new Logger({
-  name: config.name + ':server',
+  name: config.name,
   streams: [
     {
       stream: process.stdout,
@@ -13,3 +16,4 @@ module.exports = new Logger({
   ],
   serializers: restify.bunyan.serializers,
 });
+
