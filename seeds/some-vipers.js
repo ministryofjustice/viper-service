2b9a6fff0ccf84dbf84e6f0236375fc9b38ea797
Nick Redshaw <nickredshaw@gmail.com>
2017-07-14 13:49:11 +0100

    Move the CSRA test personas in their own file

--- seeds/some-vipers.js
+++ seeds/some-vipers.js
@@ -5,8 +5,5 @@ exports.seed = (knex) =>
         {nomis_id: 'A1234AA', score: 0.2181236123},
         {nomis_id: 'A1234AB', score: 0.652134534543},
         {nomis_id: 'A1234AC', score: 0.98324545745},
-        {nomis_id: 'J1234LO', score: 0.35},
-        {nomis_id: 'R6524MO', score: 0.50},
-        {nomis_id: 'I9876RA', score: 0.92},
       ])
     );
