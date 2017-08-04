b4ac0e7db788cf6df4e09d17d28edb31403283e4
mikejackson <michael.jackson@digital.justice.gov.uk>
2017-08-04 12:00:00 +0100

    Write to staging

--- bin/unzip-and-load-into-staging.js
+++ bin/unzip-and-load-into-staging.js
@@ -2,13 +2,28 @@
 
 const yauzl = require('yauzl');
 const csv = require('csv');
+const db = require('../server/db');
+const knex = db();
+
 const filename = process.argv[2];
 
 const options = {
-  columns : ['offender_id_display', 'viper'], // by default read the first line and use values found as columns
+  columns: ['offender_id_display', 'viper'], // by default read the first line and use values found as columns
 };
 
-console.log("filename is " + filename);
+console.log('filename is ' + filename);
+
+function insertRow (data) {
+
+  knex('staging')
+    .insert({nomis_id: data.offender_id_display, score: data.viper})
+    .then((success) => {
+        console.log("success");
+      },
+      (err) => {
+        console.log(err);
+      });
+}
 
 yauzl.open(filename, {lazyEntries: true}, function (err, zipfile) {
   if (err) throw err;
@@ -23,17 +38,24 @@ yauzl.open(filename, {lazyEntries: true}, function (err, zipfile) {
         });
         const parser = csv.parse(options);
         readStream.pipe(parser)
-          .on('error',function(err){
+          .on('error', function (err) {
             console.error(err);
           })
-          .on('data',function(data){
+          .on('data', function (data) {
             // outputs an object containing a set of key/value pair representing a line found in the csv file.
             console.log(data);
+            if (data.viper !== 'viper') {
+              insertRow(data);
+            }
+          })
+          .on('end', function (something) {
+            console.log("finished streaming");
           });
-          // .on('column',function(key,value){
-          //   // outputs the column name associated with the value found
-          //   console.log('#' + key ' = ' + value);
-          // })
+
+        // .on('column',function(key,value){
+        //   // outputs the column name associated with the value found
+        //   console.log('#' + key ' = ' + value);
+        // })
       });
     }
   );
