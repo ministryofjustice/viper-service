2e47e743ef184a11d5b5a141ea8953ff8c0e42d2
Glen Mailer <glenjamin@gmail.com>
2017-08-22 15:23:58 +0100

    Azure webjob task runner and slight tweak to import SQL

--- api/datasources/sql/import.sql
+++ api/datasources/sql/import.sql
@@ -1,10 +1,10 @@
-INSERT INTO staging(nomis_id, score)
-SELECT * FROM OPENROWSET(
-    BULK :filename,
+BULK INSERT staging
+FROM :filename
+WITH (
     FORMATFILE = 'import_staging_data_format.xml',
     FORMAT = 'CSV',
     FIELDQUOTE = '"',
     DATA_SOURCE = 'storageblob',
     FORMATFILE_DATA_SOURCE = 'storageblob',
     FIRSTROW = 2 -- ignore header row
-) AS "file";
+);
