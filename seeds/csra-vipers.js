1072fe2fc762e9ccb384a54afb6a6774daae4b42
Steven Bapaga <sbapaga@gmail.com>
2017-12-14 10:46:19 +0000

    Update CSRA viper seed

--- seeds/csra-vipers.js
+++ seeds/csra-vipers.js
@@ -1,11 +1,11 @@
 exports.seed = (knex) =>
   knex('scores')
-    .whereIn('nomis_id',['J1234LO','R6524MO','I9876RA'])
+    .whereIn('nomis_id',['A1401AE','A1414AE','A1421AE']) // These are NOMIS_ID's from NOTM staging environment
     .delete()
     .then(
       () => knex('scores').insert([
-        {nomis_id: 'J1234LO', score: 0.35},
-        {nomis_id: 'R6524MO', score: 0.50},
-        {nomis_id: 'I9876RA', score: 0.92},
+        { nomis_id: 'A1401AE', score: 0.35 },
+        { nomis_id: 'A1414AE', score: 0.50 },
+        { nomis_id: 'A1421AE', score: 0.92 },
       ])
     );
