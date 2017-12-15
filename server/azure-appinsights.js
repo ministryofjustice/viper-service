8dda86360df4581f62c5ad3e94da2865bd89558f
Matt Smith <mattonfoot@gmail.com>
2017-12-15 09:40:17 +0000

    minor dependency updates and major upgrade to app insights client, code changes to adapt

--- server/azure-appinsights.js
+++ server/azure-appinsights.js
@@ -2,7 +2,7 @@ if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
     const appInsights = require('applicationinsights');
     console.log("Enabling azure application insights");
     appInsights.setup().start();
-    module.exports = appInsights.client;
+    module.exports = appInsights.defaultClient;
 } else {
     module.exports = null;
 }
